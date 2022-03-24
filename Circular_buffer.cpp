600b9c38efb01e5979784f07bebc006e21f0595b
NadezhdaShilaeva <shilaeva5nadya@mail.ru>
2022-03-24 22:43:16 +0300

    update buffer

--- Circular_buffer.cpp
+++ Circular_buffer.cpp
@@ -1,172 +1,47 @@
 #include <iostream>
-
-template <typename T>
-class CCircularBuffer
-{
-public:
-    explicit CCircularBuffer(size_t size)
-        : size_(size), buffer_(new T[size + 1]), begin_(0), end_(0) {}
-    explicit CCircularBuffer(size_t size, T *buffer)
-        : size_(size), buffer_(new T[size + 1]), begin_(0), end_(size)
-    {
-        memcpy(buffer_, buffer, sizeof(T) * size_);
-    }
-    CCircularBuffer(const CCircularBuffer &other)
-        : size_(other.size_), buffer_(new T[other.size_ + 1]), begin_(other.begin_), end_(other.end_)
-    {
-        memcpy(buffer_, other.buffer, sizeof(T) * size_);
-    }
-    CCircularBuffer &operator=(const CCircularBuffer &other)
-    {
-        if (&other == this)
-        {
-            return *this;
-        }
-        size_ = other.size_;
-        delete[] buffer_;
-        buffer_ = new T[size_ + 1];
-        memcpy(buffer_, other.buffer, sizeof(T) * size_);
-        begin_ = other.begin_;
-        end_ = other.end_;
-        return *this;
-    }
-    ~CCircularBuffer()
-    {
-        delete[] buffer_;
-    }
-    void push_back(const T &value)
-    {
-        buffer_[end_] = value;
-        end_ = (end_ + 1) % (size_ + 1);
-        if (end_ == begin_)
-        {
-            begin_ = (begin_ + 1) % (size_ + 1);
-        }
-    }
-    void push_front(const T &value)
-    {
-        begin_ = (begin_ - 1) % (size_ + 1);
-        buffer_[begin_] = value;
-        if (begin_ == end_)
-        {
-            end_ = (end_ - 1) % (size_ + 1);
-        }
-    }
-    void pop_back()
-    {
-        end_ = (end_ - 1) % (size_ + 1);
-    }
-    void pop_front()
-    {
-        begin_ = (begin_ + 1) % (size_ + 1);
-    }
-    T &front() const
-    {
-        return buffer_[begin_];
-    }
-    T &back() const
-    {
-        return buffer_[end_ - 1];
-    }
-    T &operator[](size_t i) const
-    {
-        return buffer_[(begin_ + i) % (size_ + 1)];
-    }
-    void resize(size_t size)
-    {
-        CCircularBuffer new_buffer{size_ + size};
-        int i = 0;
-        for(Iterator it = this->begin(); it != this->end(); ++it)
-        {
-            new_buffer.buffer_[i] = *it;
-            ++i;
-        }
-        new_buffer.end_ = i;
-        *this = new_buffer;
-    }
-    bool empty() const
-    {
-        return begin_ == end_;
-    }
-    class Iterator;
-    Iterator begin()
-    {
-        return Iterator(this, buffer_ + begin_);
-    }
-    Iterator end()
-    {
-        return Iterator(this, buffer_ + end_);
-    }
-
-private:
-    T *buffer_;
-    size_t size_;
-    size_t begin_;
-    size_t end_;
-};
-
-template<typename T>
-class CCircularBuffer<T>::Iterator
-{
-    public:
-    Iterator(CCircularBuffer* curr, T* current)
-    :circular_buffer_(curr), current_(current) {}
-    Iterator(const Iterator &other)
-    :circular_buffer_(other.circular_buffer_), current_(other.current_) {}
-    Iterator &operator=(const Iterator& other)
-    {
-        if(*this!=other)
-        {
-            this->circular_buffer_ = other.circular_buffer_;
-            this->current_  = other.current_;
-        }
-        return *this;
-    }
-    Iterator &operator++(int)
-    {
-        Iterator tmp(*this);
-        operator++();
-        return *this;
-    }
-    Iterator &operator++()
-    {
-        ++current_;
-        if(current_ == (circular_buffer_->buffer_ + circular_buffer_->size_ + 1))
-        {
-            current_ = circular_buffer_->buffer_;
-        }
-        return *this;
-    }
-    bool operator==(const Iterator& right)
-    {
-        return current_ == right.current_;
-    }
-    bool operator!=(const Iterator& right)
-    {
-        return !(*this == right);
-    }
-    T& operator*()
-    {
-        return *current_;
-    }
-    private:
-    CCircularBuffer* circular_buffer_;
-    T* current_;
-};
+#include <algorithm>
+#include "circular_buffer.h"
 
 int main()
 {
-    CCircularBuffer<int> buf(5);
-    for (int i = 0; i<10; ++i)
+    circular_buffer<int> buf(5);
+    for (int i = 0; i < 13; ++i)
     {
-        buf.push_back(i);
+        buf.push_front(i);
     }
-    for (CCircularBuffer<int>::Iterator it = buf.begin(); it != buf.end(); ++it)
+    for (circular_buffer<int>::iterator it = buf.begin(); it != buf.end(); ++it)
     {
         std::cout << *it << " ";
     }
-    CCircularBuffer<int>::Iterator it = buf.begin();
+    std::cout << std::endl;
+    circular_buffer<int>::iterator it = buf.begin();
     *it = 10;
-    std::cout << buf[0]; 
+    std::cout << buf[0] << std::endl;
+    std::cout << *std::max_element(buf.begin(), buf.end()) << std::endl;
+    buf.resize(20);
+    for (int i = 23; i<49; ++i)
+    {
+        buf.push_back(i);
+    }
+    for (circular_buffer<int>::iterator it = buf.begin(); it != buf.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+    //std::sort(buf.begin(), buf.end());
+    for(int i = 0;  i < 7; ++i)
+    {
+        buf.pop_back();
+    }
+    for(int i = 0;  i < 5; ++i)
+    {
+        buf.pop_front();
+    }
+    for (circular_buffer<int>::iterator it = buf.begin(); it != buf.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    //std::cout << *std::lower_bound(buf.begin(), buf.end(), 38) << std::endl;
+    //std::cout << *std::upper_bound(buf.begin(), buf.end(), 39) << std::endl;
     return 0;
 }
---
+++ circular_buffer.h
@@ -0,0 +1,223 @@
+template <typename T>
+class circular_buffer
+{
+public:
+    explicit circular_buffer(size_t size)
+        : size_(size), buffer_(new T[size + 1]), begin_(0), end_(0) {}
+    explicit circular_buffer(size_t size, T *buffer)
+        : size_(size), buffer_(new T[size + 1]), begin_(0), end_(size)
+    {
+        memcpy(buffer_, buffer, sizeof(T) * size_);
+    }
+    circular_buffer(const circular_buffer &other)
+        : size_(other.size_), buffer_(new T[other.size_ + 1]), begin_(other.begin_), end_(other.end_)
+    {
+        memcpy(buffer_, other.buffer, sizeof(T) * size_);
+    }
+    circular_buffer &operator=(const circular_buffer &other)
+    {
+        if (&other == this)
+        {
+            return *this;
+        }
+        size_ = other.size_;
+        delete[] buffer_;
+        buffer_ = new T[size_ + 1];
+        memcpy(buffer_, other.buffer_, sizeof(T) * size_);
+        begin_ = other.begin_;
+        end_ = other.end_;
+        return *this;
+    }
+    ~circular_buffer()
+    {
+        delete[] buffer_;
+    }
+    void push_back(const T &value)
+    {
+        buffer_[end_] = value;
+        end_ = (end_ + 1) % (size_ + 1);
+        if (end_ == begin_)
+        {
+            begin_ = (begin_ + 1) % (size_ + 1);
+        }
+    }
+    void push_front(const T &value)
+    {
+        begin_ = (begin_ - 1 + size_ + 1) % (size_ + 1);
+        buffer_[begin_] = value;
+        if (begin_ == end_)
+        {
+            end_ = (end_ - 1 + size_ + 1) % (size_ + 1);
+        }
+    }
+    void pop_back()
+    {
+        end_ = (end_ - 1 + size_ + 1) % (size_ + 1);
+    }
+    void pop_front()
+    {
+        begin_ = (begin_ + 1) % (size_ + 1);
+    }
+    T &front() const
+    {
+        return buffer_[begin_];
+    }
+    T &back() const
+    {
+        return buffer_[(end_ - 1 + size_ + 1) % (size_ + 1)];
+    }
+    T &operator[](size_t i) const
+    {
+        return buffer_[(begin_ + i) % (size_ + 1)];
+    }
+    void resize(size_t size)
+    {
+        circular_buffer new_buffer{size};
+        int i = 0;
+        for (iterator it = this->begin(); it != this->end() && i < size; ++it)
+        {
+            new_buffer.buffer_[i] = *it;
+            ++i;
+        }
+        new_buffer.end_ = i;
+        *this = new_buffer;
+    }
+    size_t size() const
+    {
+        if (begin_ < end_)
+        {
+            return end_ - begin_;
+        }
+        else
+        {
+            return size_ + 1 - begin_ + end_;
+        }
+    }
+    size_t capacity()
+    {
+        return size_;
+    }
+    bool empty() const
+    {
+        return begin_ == end_;
+    }
+    class iterator;
+    iterator begin()
+    {
+        return iterator(this, buffer_ + begin_);
+    }
+    iterator end()
+    {
+        return iterator(this, buffer_ + end_);
+    }
+
+private:
+    T *buffer_;
+    size_t size_;
+    size_t begin_;
+    size_t end_;
+};
+
+template <typename T>
+class circular_buffer<T>::iterator
+{
+public:
+    iterator(circular_buffer *curr, T *current)
+        : circular_buffer_(curr), current_(current) {}
+    iterator(const iterator &other)
+        : circular_buffer_(other.circular_buffer_), current_(other.current_) {}
+
+    iterator &operator=(const iterator &other)
+    {
+        if (*this != other)
+        {
+            this->circular_buffer_ = other.circular_buffer_;
+            this->current_ = other.current_;
+        }
+        return *this;
+    }
+    iterator &operator++(int)
+    {
+        iterator tmp(*this);
+        operator++();
+        return *this;
+    }
+    iterator &operator++()
+    {
+        ++current_;
+        if (current_ == (circular_buffer_->buffer_ + circular_buffer_->size_ + 1))
+        {
+            current_ = circular_buffer_->buffer_;
+        }
+        return *this;
+    }
+    iterator &operator--(int)
+    {
+        iterator tmp(*this);
+        operator--();
+        return *this;
+    }
+    iterator &operator--()
+    {
+        if (current_ == circular_buffer_->buffer_)
+        {
+            current_ = circular_buffer_->buffer_ + circular_buffer_->size_;
+        }
+        else
+        {
+            --current_;
+        }
+        return *this;
+    }
+    iterator &operator+(int right)
+    {
+        for (int i = 0; i < right; ++i)
+        {
+            operator++();
+        }
+        return *this;
+    }
+    iterator &operator-(int right)
+    {
+        for (int i = 0; i < right; ++i)
+        {
+            operator--();
+        }
+        return *this;
+    }
+    int operator-(const iterator &right)
+    {
+        if (current_ > right.current_)
+        {
+            return current_ - right.current_;
+        }
+        else
+        {
+            return circular_buffer_->buffer_ + circular_buffer_->size_ + 1 - right.current_ + (current_ - circular_buffer_->buffer_);
+        }
+    }
+    bool operator==(const iterator &right)
+    {
+        return current_ == right.current_;
+    }
+    bool operator!=(const iterator &right)
+    {
+        return !(*this == right);
+    }
+    bool operator<(const iterator &right)
+    {
+        return (right - *this) > 0;
+    }
+    bool operator>(const iterator &right)
+    {
+        return (*this - right) > 0;
+    }
+    T &operator*()
+    {
+        return *current_;
+    }
+
+private:
+    circular_buffer *circular_buffer_;
+    T *current_;
+};
